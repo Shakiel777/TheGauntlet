55fe278683864c1ac5ff2a26784b4aa7e06268b8
DavidRood <shakiel@roadrunner.com>
2017-03-16 19:45:34 -0400

    TG16 - ChooseNextWaypoint reading first index 0

--- Source/TheGauntlet/Public/ChooseNextWaypoint.h
+++ Source/TheGauntlet/Public/ChooseNextWaypoint.h
@@ -17,6 +17,8 @@ class THEGAUNTLET_API UChooseNextWaypoint : public UBTTaskNode
 
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	
-	
-	
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector IndexKey;
+
 };
