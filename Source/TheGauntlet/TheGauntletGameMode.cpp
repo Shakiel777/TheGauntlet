e9345fb3ba0c866437f949fe50349387a4b12cfc
DavidRood <shakiel@roadrunner.com>
2017-05-09 22:11:43 -0400

    TG-28 - Refactor Complete - Blueprint errors, but runs

--- Source/TheGauntlet/TheGauntletGameMode.cpp
+++ Source/TheGauntlet/TheGauntletGameMode.cpp
@@ -3,13 +3,13 @@
 #include "TheGauntlet.h"
 #include "TheGauntletGameMode.h"
 #include "TheGauntletHUD.h"
-#include "TheGauntletCharacter.h"
+#include "Player/FirstPersonCharacter.h"
 
 ATheGauntletGameMode::ATheGauntletGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behavior/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
